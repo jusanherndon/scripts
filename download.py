a37bc78429c06c015531afc9c00180ef135c5958
Justin Herndon <jherndon111@gmail.com>
2023-01-20 14:17:12 -0600

    the script has a base

--- download.py
+++ download.py
@@ -8,12 +8,10 @@ url = "https://subsplease.org/rss/?t&r=1080"
 xml_rss_feed = requests.get(url)
 with open('shows.xml','wb') as xml:
     xml.write(xml_rss_feed.content)
-xml.close()
 
 # Reimporting xml and translating it into a dict to be searchable
 with open('shows.xml') as test:
     xml_dict = xmltodict.parse(test.read())
-test.close()
 
 #automatically removes the xml file so space is not wasted on the device
 #os.remove('shows.xml')
@@ -34,13 +32,13 @@ with open("shows.txt", 'r') as shows:
         for num in range(0, len(xml_dict['rss']['channel']['item'])):
             if show.strip() in show_list[num]:
                 torrent_file_link = requests.get(xml_dict['rss']['channel']['item'][num]['link'])
-                show_names.append(show.strip())
-                trackers.update({show.strip():torrent_file_link.content})
-shows.close()
+                show_names.append(show_list[num].strip())
+                trackers.update({show_list[num].strip():torrent_file_link.content})
 
-with("{}.torrent".format(show_names[0])) as torrent:
-    for show_name in show_names:
-        torrent.write(trackers[show_name])
+for name in show_names:
+    with open(f"{name}.torrent", "wb") as torrent_file:
+        torrent_file.write(trackers[name])
+
         #torrent.write(trackers[show_name])
     #torrent.close()
 #"{}.torrent".format(show_name),
