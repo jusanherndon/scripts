b3c327921faeb9206ea411a94ac41faa688b2555
Justin Herndon <jherndon111@gmail.com>
2024-04-30 18:24:40 -0500

    The script works and cleans up after itself. Last step is to make a cronjob

--- transcoding/transcoding.py
+++ transcoding/transcoding.py
@@ -15,23 +15,23 @@ with open(show_path, 'r') as shows:
         # Match on just the show name so that was the file name looks nicer for the jellyfin server
         show_name_and_episode = re.findall("(?<=\ )(.*?)(?=\()", show.strip())
         transcoded_show_name = str(show_name_and_episode[0] + ".mkv").replace("-", " ").replace(" ", "_" )
-        print(transcoded_show_name)
 
         # copy over the video file to this pc
-        scp_video_cmd = f"scp justin@192.168.1.115:/mnt/deluge/'{show.strip()}' /home/justin/{transcoded_show_name}"
-        process = subprocess.run(scp_video_cmd, shell=True, check=True)
+        #scp_video_cmd = f"scp justin@192.168.1.115:/mnt/deluge/'{show.strip()}' /home/justin/{transcoded_show_name}"
+        #scp_video_cmd = f"scp justin@192.168.1.115:/mnt/jellyfin/shows/'{show.strip()}' /home/justin/{transcoded_show_name}"
+        #process = subprocess.run(scp_video_cmd, shell=True, check=True)
 
         # step 3: Transcode the video using the old video as its base
 
-        ffmpeg_cmd = f'ffmpeg -vaapi_device /dev/dri/renderD128 -i {transcoded_show_name} -c:v hevc_amf -x265-params "pass=1:lossless=1" -an -f null /dev/null && ffmpeg -vaapi_device /dev/dri/renderD128 -i {transcoded_show_name} -c:v hevc_amf -c:a aac -map_metadata 0:g -x265-params "pass=2:lossless=1" transcoded_{transcoded_show_name}'
-        process = subprocess.run(ffmpeg_cmd, shell=True, check=True)
+        #ffmpeg_cmd = f'ffmpeg -vaapi_device /dev/dri/renderD128 -i {transcoded_show_name} -c:v hevc_amf -x265-params "pass=1:lossless=1" -an -f null /dev/null && ffmpeg -vaapi_device /dev/dri/renderD128 -i {transcoded_show_name} -c:v hevc_amf -c:a aac -map_metadata 0:g -x265-params "pass=2:lossless=1" transcoded_{transcoded_show_name}'
+        #process = subprocess.run(ffmpeg_cmd, shell=True, check=True)
 
         # step 4: send new video file to the jellyfin media server
-        scp_transcode_video_cmd = f"scp /home/justin/transcoded_{transcoded_show_name} justin@192.168.1.115:/mnt/jellfin/shows/transcoded_{transcoded_show_name}"
+        scp_transcode_video_cmd = f"scp transcoded_{transcoded_show_name} justin@192.168.1.115:/mnt/jellyfin/shows"
         process = subprocess.run(scp_transcode_video_cmd, shell=True, check=True)
         
         # step 5 clean up any left over files
         os.remove("show_names.txt")
-        os.remove(f"'{show.strip()}'")
+        os.remove(f'{transcoded_show_name}')
         os.remove(f"transcoded_{transcoded_show_name}")
 
