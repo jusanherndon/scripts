7a09c3374895286ab48c0cacf7b61cb17d917077
Justin Herndon <jherndon111@gmail.com>
2024-04-30 17:51:48 -0500

    Everything seems to work, but the final ssh and delettion of extra parts

--- transcoding/transcoding.py
+++ transcoding/transcoding.py
@@ -2,28 +2,34 @@ import os
 import subprocess
 import re
 
-# step 1: send video file or files with input list to this directory
+# step 1: send show_names.txt to this machine
 
-
+scp_cmd = "scp justin@192.168.1.115:/home/justin/show_names.txt /home/justin/show_names.txt"
+process = subprocess.run(scp_cmd, shell=True, check=True)
 
 # step 2: take all needed inputs for this script
 
 #shows_path = os.path.abspath("/home/justin/git/scripts/transcoding/shows.txt")
-show_path = os.path.abspath("/home/justin/git/shows.txt")
+show_path = os.path.abspath("/home/justin/show_names.txt")
 with open(show_path, 'r') as shows:
     for show in shows.readlines():
-        
+
         # Match on just the show name so that was the file name looks nicer for the jellyfin server
-        show_name_and_episode = re.findall("(?<=\])(.*?)(?=\()", show.strip())
-        transcoded_show_name = str(show_name_and_episode[0]).replace("-", " ").replace(" ", "_" )
+        show_name_and_episode = re.findall("(?<=\ )(.*?)(?=\()", show.strip())
+        transcoded_show_name = str(show_name_and_episode[0] + ".mkv").replace("-", " ").replace(" ", "_" )
+        print(transcoded_show_name)
+
+        # copy over the video file to this pc
+        scp_video_cmd = f"scp justin@192.168.1.115:/mnt/jellyfin/shows/'{show.strip()}' /home/justin/{transcoded_show_name}"
+        process = subprocess.run(scp_video_cmd, shell=True, check=True)
 
         # step 3: Transcode the video using the old video as its base
 
-        ffmpeg_cmd = f'ffmpeg -vaapi_device /dev/dri/renderD128 -i {show} -c:v hevc_amf -x265-params "pass=1:lossless=1" -an -f null /dev/null && ffmpeg -vaapi_device /dev/dri/renderD128 -i {show} -c:v hevc_amf -c:a aac -map_metadata 0:g -x265-params "pass=2:lossless=1" {transcoded_show_name}.mkv'
-        process = subprocess.Popen(ffprobe_cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True)
+        ffmpeg_cmd = f'ffmpeg -vaapi_device /dev/dri/renderD128 -i {transcoded_show_name} -c:v hevc_amf -x265-params "pass=1:lossless=1" -an -f null /dev/null && ffmpeg -vaapi_device /dev/dri/renderD128 -i {transcoded_show_name} -c:v hevc_amf -c:a aac -map_metadata 0:g -x265-params "pass=2:lossless=1" transcoded_{transcoded_show_name}'
+        process = subprocess.run(ffmpeg_cmd, shell=True, check=True)
 
         # step 4: send new video file to the jellyfin media server
-
+        
 
         # step 5 clean up any left over files
 
