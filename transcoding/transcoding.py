740469ffdf5c6e6db3fe1d280ff28ae07f9c91a6
Justin Herndon <jherndon111@gmail.com>
2024-05-01 17:15:47 -0500

    forgot to remove some transcodings

--- transcoding/transcoding.py
+++ transcoding/transcoding.py
@@ -23,11 +23,11 @@ with open(show_path, 'r') as shows:
 
         # step 3: Transcode the video using the old video as its base
 
-        ffmpeg_cmd = f'ffmpeg -vaapi_device /dev/dri/renderD128 -i {show_name} -c:v hevc_amf -x265-params "pass=1:lossless=1" -an -f null /dev/null && ffmpeg -vaapi_device /dev/dri/renderD128 -i {show_name} -c:v hevc_amf -c:a aac -map_metadata 0:g -x265-params "pass=2:lossless=1" transcoded_{transcoded_show_name}'
+        ffmpeg_cmd = f'ffmpeg -vaapi_device /dev/dri/renderD128 -i {show_name} -c:v hevc_amf -x265-params "pass=1:lossless=1" -an -f null /dev/null && ffmpeg -vaapi_device /dev/dri/renderD128 -i {show_name} -c:v hevc_amf -c:a aac -map_metadata 0:g -x265-params "pass=2:lossless=1" {transcoded_show_name}'
         process = subprocess.run(ffmpeg_cmd, shell=True, check=True)
 
         # step 4: send new video file to the jellyfin media server
-        scp_transcode_video_cmd = f"scp transcoded_{transcoded_show_name} justin@192.168.1.115:/mnt/jellyfin/shows"
+        scp_transcode_video_cmd = f"scp {transcoded_show_name} justin@192.168.1.115:/mnt/jellyfin/shows"
         process = subprocess.run(scp_transcode_video_cmd, shell=True, check=True)
         
         # step 5 clean up any left over files
